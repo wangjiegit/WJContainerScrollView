11bb0a928668b16ecc4e6c8cf31cb0a6f34c281c
wangjiegit <247034053@qq.com>
2025-07-30 10:43:18 +0800

    Update WJSwitchPagesView.m

--- WJContainerScrollView/WJSwitchPagesView.m
+++ WJContainerScrollView/WJSwitchPagesView.m
@@ -81,14 +81,12 @@ typedef NS_ENUM(NSInteger, WJSwitchPagesDirection) {
 
 //判断展示左边的视图还是右边的视图
 - (void)scrollViewPan:(UIPanGestureRecognizer *)pgr {
-    if (pgr.state == UIGestureRecognizerStateBegan) {
-        CGPoint velocity = [pgr velocityInView:pgr.view];
-        if (velocity.x == 0) return;
-        if (velocity.x > 0) {
-            self.direction = WJSwitchPagesDirectionLeft;
-        } else {
-            self.direction = WJSwitchPagesDirectionRight;
-        }
+    CGPoint velocity = [pgr velocityInView:pgr.view];
+    if (velocity.x == 0) return;
+    if (velocity.x > 0) {
+        self.direction = WJSwitchPagesDirectionLeft;
+    } else {
+        self.direction = WJSwitchPagesDirectionRight;
     }
 }
 
